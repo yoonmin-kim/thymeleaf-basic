458e9f88af470be45e998870cfb049a1e7d518a3
YoonMin <rladbsals23@gmail.com>
2021-07-07 21:13:05 +0900

    feat : 템플릿 레이아웃1
    정해놓은 태그를 가져와서 사용하는 템플릿조각과는 다르게 일부 태그는 가져올때
    다른 태그로 바꾸고 싶을때가 있다.
    예를들어 페이지 A,B,C .html 에서 공통 템플릿으로 base.html 을 사용하고있다.
    
    A,B,C 각페이지에서 타이틀태그가 다 달랐으면 하는 요구사항이 있을 수 있다.
    그럼 각페이지에서 base.html을 선언할때 ~{::title}을 사용하여 따로 정의한 title
    태그를 base.html에 넘겨서 사용할 수 있다.

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -12,4 +12,9 @@ public class TemplateController {
     public String fragment() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
 }
